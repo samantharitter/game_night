971f5f3729d4c7d2d0e605c85a93344694b08530
Samantha Ritter <samantha.ritter@10gen.com>
2014-07-14 19:37:23 -0400

    Add team leaderboard

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -7,4 +7,15 @@ class TeamsController < ApplicationController
     # for each player, see if the player already exists, and if so, add them.
     # if not, create new player.
   end
+
+  def index
+    @teams_by_score = teams_by_score
+  end
+
+  def teams_by_score
+    Team.all.sort do |a, b|
+      b.score <=> a.score
+    end
+  end
+  helper_method :teams_by_score
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -8,13 +8,6 @@ class Team
 
   # Get this team's score
   def score
-    Plays.where({:team_id => self._id}).sum(:points)
-  end
-
-  # Return an array of teams sorted by score in descending order.
-  def self.teams_by_score
-    Teams.all.sort do |a, b|
-      b.score <=> a.score
-    end
+    Play.where({:team_id => self._id}).sum(:points)
   end
 end
